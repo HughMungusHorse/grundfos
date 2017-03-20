8bd8a895064ebb9e197071a4e39f190411e4e5f2
HughMungusHorse <kavi1989@gmail.com>
2017-03-20 10:00:46 +0100

    Repository oprettet
    begyndt på html css etc

---
+++ formular.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Grundfos Project</title>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+
+<body>
+    <header>
+    <img class="logo" src="img/logo.png" alt="grundfos green logo">
+    </header>
+    
+    
+    
+    
+    
+</body>
+
+</html>
